2d974cbe0a22afd813dfe01bd8bd30451a22228b
rejector7 <2643719428@qq.com>
2017-06-26 17:35:02 +0800

    user entity

---
+++ code/questionnaire/java/model/UserProfile.java
@@ -0,0 +1,24 @@
+package model;
+
+public class UserProfile {
+	private String id;
+	
+	private String profile;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getProfile() {
+		return profile;
+	}
+
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+	
+}
