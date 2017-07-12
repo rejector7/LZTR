0349bd39c5497cb812cc90f76f63c2c0cd6af88f
tangzhongyue <853752063@qq.com>
2017-07-12 17:06:11 +0800

    Add Answer service and action for CRUD

--- code/questionnaire/src/main/java/model/AnswerSheet.java
+++ code/questionnaire/src/main/java/model/AnswerSheet.java
@@ -1,7 +1,12 @@
 package model;
 
 import org.springframework.data.mongodb.core.mapping.Document;
-
+/**
+ * Model for answer sheet, including the default id created by mongodb, the id of the answer sheet, 
+ * the id of the user who gave the answers(if he/she was logged on), and the answers he/she gave
+ * @author LZTR
+ *
+ */
 @Document(collection="AnswerSheets")
 public class AnswerSheet {
 	private int id;
@@ -9,13 +14,34 @@ public class AnswerSheet {
 	private int userid;
 	private String content;
 	public AnswerSheet() {
-		super();
 	}
+	/**
+	 * Create answersheet with specific content, usually used when a new answersheet, which is 
+	 * done by a unregistered user, is being saved for the first time
+	 * the firsttime
+	 * @param content
+	 */
+	public AnswerSheet(String content) {
+		super();
+		this.content = content;
+	}
+	/**
+	 * Create answersheet with specific answerid and content,usually used when answerid is known
+	 * @param answerid
+	 * @param content
+	 */
 	public AnswerSheet(int answerid, String content) {
 		super();
 		this.answerid = answerid;
 		this.content = content;
 	}
+/**
+ * Create answersheet with specific answerid, userid and content,usually used when answerid is known
+ * and there exists a userid
+ * @param answerid
+ * @param userid
+ * @param content
+ */
 	public AnswerSheet(int answerid, int userid, String content) {
 		super();
 		this.answerid = answerid;
--- code/questionnaire/src/main/java/service/impl/AnswerSheetServiceImpl.java
+++ code/questionnaire/src/main/java/service/impl/AnswerSheetServiceImpl.java
@@ -1,5 +1,71 @@
 package service.impl;
 
-public class AnswerSheetServiceImpl {
+import java.util.List;
+
+import dao.AnswerDao;
+import dao.AnswerSheetDao;
+import model.Answer;
+import model.AnswerSheet;
+import service.AnswerSheetService;
+
+public class AnswerSheetServiceImpl implements AnswerSheetService {
+	private AnswerDao ansDao;
+	private AnswerSheetDao anssheetDao;
+	public void setAnsDao(AnswerDao ansDao) {
+		this.ansDao = ansDao;
+	}
+	public void setAnssheetDao(AnswerSheetDao anssheetDao) {
+		this.anssheetDao = anssheetDao;
+	}
+	/* (non-Javadoc)
+	 * @see service.impl.AnswerSheetService#saveAnswer(model.Answer, model.AnswerSheet)
+	 */
+	@Override
+	public void addAnswer(Answer ans, AnswerSheet anst){
+		int id = ansDao.addAnswer(ans);
+		anst.setAnswerid(id);
+		anssheetDao.addAnswerSheet(anst);
+	}
 	
+	/* (non-Javadoc)
+	 * @see service.impl.AnswerSheetService#updateAnswer(model.Answer, model.AnswerSheet)
+	 */
+	@Override
+	public void updateAnswer(Answer ans, AnswerSheet anst){
+		ansDao.updateAnswer(ans);
+		anssheetDao.updateAnswerSheet(anst);
+	}
+	
+	/* (non-Javadoc)
+	 * @see service.impl.AnswerSheetService#deleteAnswer(model.Answer, model.AnswerSheet)
+	 */
+	@Override
+	public void deleteAnswer(Answer ans, AnswerSheet anst){
+		ansDao.deleteAnswer(ans);
+		anssheetDao.deleteAnswerSheet(anst);
+	}
+	
+	/* (non-Javadoc)
+	 * @see service.impl.AnswerSheetService#getAnswerById(int)
+	 */
+	@Override
+	public Answer getAnswerById(int id){
+		return ansDao.getAnswerById(id);
+	}
+	
+	/* (non-Javadoc)
+	 * @see service.impl.AnswerSheetService#getAnswerSheetById(int)
+	 */
+	@Override
+	public AnswerSheet getAnswerSheetById(int id){
+		return anssheetDao.getAnswerSheetById(id);
+	}
+	
+	/* (non-Javadoc)
+	 * @see service.impl.AnswerSheetService#getAnswerByQuestion(int)
+	 */
+	@Override
+	public List<Answer> getAnswerByQuestion(int quesid){
+		return ansDao.getAnswersByQuesId(quesid);
+	}
 }
